7f038bb94003f17101eb6e2af7b4574af552fd63
Miki <uxy19498@idxue.com>
2020-04-22 19:05:20 +0800

    Initial Commit

---
+++ AccessCodeSuccessScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccessCodeSuccessScreen
+//
+//  Created by Mojal, Michelle Kim dela Pena on 22/04/2020.
+//  Copyright © 2020 DCPDC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
